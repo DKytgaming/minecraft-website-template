2cd759395751524adc95cab8a54fc1add0e8df4f
DKytgaming <101945037+DKytgaming@users.noreply.github.com>
2023-12-24 20:37:28 +0530

    Update server-info.js

--- server-info.js
+++ server-info.js
@@ -1,6 +1,6 @@
 // fetch("https://api.minetools.eu/ping/YourServerIP/Port") and without this two things do not edit anything or you may messup the code
 
-fetch("https://api.minetools.eu/ping/play.hypixel.net/25565").then((discord1)=>{
+fetch("https://api.minetools.eu/ping/play.gamerweb.online/25565").then((discord1)=>{
     console.log(discord1.status);
     return discord1.json();
 
